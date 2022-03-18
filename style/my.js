c5d5276b2e94f533b01c2a46e275622e89c8190d
Antanid <dbalakleenko1@gmail.com>
2022-03-18 16:12:59 +0200

    add menu burger

---
+++ style/my.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active')
+    })
+}
