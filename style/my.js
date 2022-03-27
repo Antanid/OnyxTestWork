89d11254a617af61db6071d74a86f4f28456fe2d
Antanid <dbalakleenko1@gmail.com>
2022-03-27 16:38:14 +0300

    add PopUp

--- style/my.js
+++ style/my.js
@@ -7,3 +7,28 @@ if (iconMenu) {
         menuBody.classList.toggle('_active')
     })
 }
+
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.popup_open');
+let closePopupButtons = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+})
+
+closePopupButtons.addEventListener('click', (e) =>{
+    popupBg.classList.remove('active');
+    popupBg.classList.remove('active');
+})
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popupBg.classList.remove('active');
+    }
+})
