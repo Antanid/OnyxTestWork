aa9755311d1a86bac1f389ec06408da8d4bd7014
Antanid <dbalakleenko1@gmail.com>
2022-03-31 17:12:32 +0300

    add background slider

--- style/my.js
+++ style/my.js
@@ -78,3 +78,26 @@ document.addEventListener('click', (e) => {
         popupExc.classList.remove('active');
     }
 })
+
+const btns = document.querySelectorAll('.nav-btn');
+const slides = document.querySelectorAll('.video-slide');
+
+let sliderNav = function (manual) {
+
+    btns.forEach((btn) => {
+        btn.classList.remove('active');
+    })
+
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    })
+
+    btns[manual].classList.add('active');
+    slides[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) =>{
+    btn.addEventListener('click', (e) =>{
+        sliderNav(i);
+    })
+})
